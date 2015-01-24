e39819a59c22968531c4ae372e9c3fd789dc9644
ozsayag <ozsayag@gmail.com>
2015-01-25 00:51:22 +0200

    adding cli args

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-require('../');
+var version = require('../package.json').version;
+
+var argv = require('yargs')
+    .options('p', {
+        alias : 'port',
+        default : 80,
+        describe: 'The server\'s port'
+    })
+    .options('l', {
+        default : false,
+        describe: 'Launching browser'
+    })
+    .help('h').alias('h', 'help')
+    .version(version, 'v').alias('v', 'version')
+    .usage('Starts the cordova simulator.\n' + 
+           'Usage: cordova-simulator [-l] [-p num | --port=num] [folder]\n' +
+           'Where folder is the folder containing the index.html file of your app\n' +
+           'Default folder is www/ from the current directory.')
+    .argv;
+
+var run = require('../');
+
+run({
+    dir: argv._[0],
+    port: argv.port,
+    launch: argv.l
+});
--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 var path = require('path');
 
 var express = require('express.io');
+var open = require("open");
 
 var reload = require('./lib/reload');
 
-var app = express();
-var dir = 'www';
-app.http().io();
+module.exports = function(obj) {
+    var dir = obj.dir || 'www';
+    var launch = obj.launch == true;
+    var port = obj.port || 80;
+    
+    var app = express();
+    app.http().io();
 
-app.get('/www/cordova.js', function (req, res) {
-    res.set('Content-Type', 'application/javascript');
-    res.end();
-});
+    app.get('/www/cordova.js', function (req, res) {
+        res.set('Content-Type', 'application/javascript');
+        res.end();
+    });
 
-app.use('/www', express.static(dir));
+    app.use('/www', express.static(dir));
 
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
 
-reload(app, dir);
-app.listen(80);
+    reload(app, dir);
+    app.listen(port);
+    
+    if (launch) {
+        open("http://localhost:" + port);
+    }
+}
